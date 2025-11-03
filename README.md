5202c27a019d55c23a4f92d5612a25e1b2a28d1f
Rafael Trindade <rafatrindade.exe@gmail.com>
2025-11-03 00:32:40 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# oncoped-data-br
-Monitoramento de atendimentos, repasses públicos e estrutura hospitalar voltados à oncologia pediátrica no Brasil, integrando dados do DATASUS, INCA, CNES e Portal da Transparência.
+# 🎗️ oncoped-data-br 
+`em desenvolvimento`
+
+Monitoramento de casos, atendimentos, repasses públicos e estrutura hospitalar voltados à oncologia pediátrica no Brasil, integrando dados do DATASUS, INCA, CNES e Portal da Transparência.
+
+## 📄 Relatório de Execução do Projeto:
+
+`dbc_to_csv.py` 
+- ✅ Download dos arquivos `.dbc` do DATASUS (Painel de Oncologia 2016~2025) e utilização do executável `dbf2dbc.exe` (fornecido pelo DATASUS/TabWin para expansão de arquivos DBC) para conversão automatizada para `.csv`, com filtragem de registros de idade ≤ 18 anos.
+- ✅ Unificação de arquivos intermediários e organização dos dados exportados em `data/raw/datasus_oncologia_infantil.csv`.
+
+---
+
+## 📦 Bibliotecas Utilizadas:
+
+| Pacote            | Versão      | Observação |
+|-------------------|------------|------------|
+| **pandas**        | 2.3.3      | Manipulação e transformação de dados |
+| **dbfread**       | 2.0.7      | Leitura de arquivos `.DBF` gerados pelo DATASUS |
+
+**dbf2dbc.exe** - Executável fornecido pelo DATASUS/TabWin para expansão de arquivos `.DBC` para `.DBF` 
+
+
---
+++ scripts/dbc_to_csv.py
@@ -0,0 +1,51 @@
+import os
+import subprocess
+from dbfread import DBF
+import pandas as pd
+
+idade_maxima = 18
+
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # raiz do projeto
+dbc_dir = os.path.join(base_dir, 'data', 'utils/datasus_dbc')
+csv_dir = os.path.join(base_dir, 'data', 'raw')
+
+dbc2dbf_exe = os.path.join(dbc_dir, 'dbf2dbc.exe')
+
+os.makedirs(csv_dir, exist_ok=True)
+
+arquivos_dbc = [f for f in os.listdir(dbc_dir) if f.lower().endswith('.dbc')]
+
+dfs_infantil = []
+
+if not arquivos_dbc:
+    print("⚠️ Nenhum arquivo .DBC encontrado em data/dbc.")
+else:
+    for arquivo in arquivos_dbc:
+        caminho_dbc = os.path.join(dbc_dir, arquivo)
+        nome_base = os.path.splitext(arquivo)[0]
+        caminho_dbf = os.path.join(dbc_dir, f"{nome_base}.dbf")
+
+        print(f"\nConvertendo {arquivo} para DBF...")
+        subprocess.run([dbc2dbf_exe, caminho_dbc], check=True, cwd=dbc_dir)
+
+        print(f"Lendo {nome_base}.dbf e filtrando registros de crianças...")
+        tabela = DBF(caminho_dbf, encoding='latin-1')
+        df = pd.DataFrame(iter(tabela))
+
+        df['IDADE'] = pd.to_numeric(df['IDADE'], errors='coerce')
+
+        df = df[df['IDADE'].notnull() & (df['IDADE'] <= idade_maxima)]
+
+        dfs_infantil.append(df)
+
+        if os.path.exists(caminho_dbf):
+            os.remove(caminho_dbf)
+
+    if dfs_infantil:
+        df_final = pd.concat(dfs_infantil, ignore_index=True)
+        caminho_final = os.path.join(csv_dir, 'datasus_oncologia_infantil.csv')
+
+        df_final.to_csv(caminho_final, index=False, encoding='utf-8')
+        print(f"\nTodos os CSVs foram unidos em: {caminho_final}")
+    else:
+        print("Nenhum registro infantil encontrado nos arquivos DBC.")
